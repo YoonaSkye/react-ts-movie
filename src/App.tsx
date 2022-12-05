496cdf3b32c1456d39d5ebbd83e4b58c28d297a4
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-12-05 20:06:52 +0800

    chore: 配置scss基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       Hello, React Movie!
+
     </div>
   );
 }
