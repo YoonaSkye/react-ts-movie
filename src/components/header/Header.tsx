f948c022b037713450ef799478368df65ded2912
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-12-06 17:04:01 +0800

    feat: 实现Header组件(待实现吸顶效果)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+
+// style && images
+import './header.scss'
+import Logo from '../../assets/tmovie.png'
+
+const headerNav = [
+  {
+      display: 'Home',
+      path: '/'
+  },
+  {
+      display: 'Movies',
+      path: '/movie'
+  },
+  {
+      display: 'TV Series',
+      path: '/tv'
+  }
+];
 
 const Header = () => {
+  const headerRef = useRef<HTMLDivElement>(null)
+
+  // 处理header吸顶效果
+
   return (
-    <div>Header</div>
+    <div className='header' ref={headerRef}>
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <Link to='/'>MyMovies</Link>
+        </div>
+        <ul className="header__nav">
+          {
+            headerNav.map((item, index)=> (
+              <li key={index}>
+                <Link to={item.path}>{item.display}</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
