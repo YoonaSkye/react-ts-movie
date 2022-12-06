cf50045b0139b98d9240fc048e52fd8672ecba0d
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-12-06 15:19:56 +0800

    feat: 配置页面路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-
+import { Routes, Route } from 'react-router-dom';
+// components
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Detail from './pages/detail/Detail';
+// style
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello, React Movie!
-
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path=':category' element={<Catalog />} />
+        <Route path=':category/:id' element={<Detail />} />
+        <Route path=':category/search/:keyword' element={<Catalog />} />
+      </Route>
+    </Routes>
   );
 }
 
