ca47e5f2a2bf749869abd51b7066a951c34aeb07
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-12-06 19:58:51 +0800

    fix: 部分增加api返回数据类型约束

---
+++ src/api/api.d.ts
@@ -0,0 +1,63 @@
+type Category = 'movie' | 'tv';
+type MovieType = 'upcoming' | 'popular' | 'top_rated';
+type TvType = 'popular' | 'top_rated' | 'on_the_air';
+
+interface MovieObject {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TvObject {
+  backdrop_path: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface VideoObject {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+interface List<T> {
+  results: Partial<T>;
+}
+
+export {
+  Category,
+  MovieType,
+  TvType,
+  MovieObject,
+  TvObject,
+  VideoObject,
+  List,
+};
--- src/api/tmdbApi.ts
+++ src/api/tmdbApi.ts
@@ -1,55 +1,60 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient';
+import {
+  Category,
+  MovieType,
+  TvType,
+  MovieObject,
+  TvObject,
+  VideoObject,
+  List,
+} from './api';
 
-type Category = 'movie' | 'tv'
-type MovieType = 'upcoming' | 'popular' | 'top_rated'
-type TvType = 'popular' | 'top_rated' | 'on_the_air'
+// export const category = {
+//   movie: 'movie',
+//   tv: 'tv',
+// };
 
-export const category = {
-    movie: 'movie',
-    tv: 'tv'
-}
+// export const movieType = {
+//   upcoming: 'upcoming',
+//   popular: 'popular',
+//   top_rated: 'top_rated',
+// };
 
-export const movieType = {
-    upcoming: 'upcoming',
-    popular: 'popular',
-    top_rated: 'top_rated'
-}
-
-export const tvType = {
-    popular: 'popular',
-    top_rated: 'top_rated',
-    on_the_air: 'on_the_air'
-}
+// export const tvType = {
+//   popular: 'popular',
+//   top_rated: 'top_rated',
+//   on_the_air: 'on_the_air',
+// };
 
 const tmdbApi = {
-    getMoviesList: (type: MovieType, params: {}) => {
-        const url = 'movie/' + movieType[type];
-        return axiosClient.get(url, params);
-    },
-    getTvList: (type: TvType, params: {}) => {
-        const url = 'tv/' + tvType[type];
-        return axiosClient.get(url, params);
-    },
-    getVideos: (cate: Category, id: number) => {
-        const url = category[cate] + '/' + id + '/videos';
-        return axiosClient.get(url, {params: {}});
-    },
-    search: (cate: Category, params: {}) => {
-        const url = 'search/' + category[cate];
-        return axiosClient.get(url, params);
-    },
-    detail: (cate: Category, id: number, params: {}) => {
-        const url = category[cate] + '/' + id;
-        return axiosClient.get(url, params);
-    },
-    credits: (cate: Category, id: number) => {
-        const url = category[cate] + '/' + id + '/credits';
-        return axiosClient.get(url, {params: {}});
-    },
-    similar: (cate: Category, id: number) => {
-        const url = category[cate] + '/' + id + '/similar';
-        return axiosClient.get(url, {params: {}});
-    },
-}
+  getMoviesList: (type: MovieType, params: {}): Promise<List<MovieObject>> => {
+    const url = 'movie/' + type;
+    return axiosClient.get(url, params);
+  },
+  getTvList: (type: TvType, params: {}): Promise<List<TvObject>> => {
+    const url = 'tv/' + type;
+    return axiosClient.get(url, params);
+  },
+  getVideos: (cate: Category, id: number): Promise<List<VideoObject>> => {
+    const url = cate + '/' + id + '/videos';
+    return axiosClient.get(url, { params: {} });
+  },
+  search: (cate: Category, params: {}) => {
+    const url = 'search/' + cate;
+    return axiosClient.get(url, params);
+  },
+  detail: (cate: Category, id: number, params: {}) => {
+    const url = cate + '/' + id;
+    return axiosClient.get(url, params);
+  },
+  credits: (cate: Category, id: number) => {
+    const url = cate + '/' + id + '/credits';
+    return axiosClient.get(url, { params: {} });
+  },
+  similar: (cate: Category, id: number) => {
+    const url = cate + '/' + id + '/similar';
+    return axiosClient.get(url, { params: {} });
+  },
+};
 
 export default tmdbApi;
